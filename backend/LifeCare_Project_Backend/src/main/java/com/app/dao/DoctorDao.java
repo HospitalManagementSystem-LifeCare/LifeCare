3ca3a47d5dbd86de8f8e707985e8b3a4c46eee53
rahul-dac-518 <rahulk.scoe.entc@gmail.com>
2024-02-11 18:31:55 +0530

    removed all controllers which were written according to old requirement

--- backend/LifeCare_Project_Backend/src/main/java/com/app/dao/DoctorDao.java
+++
@@ -1,9 +0,0 @@
-package com.app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.entities.Doctor;
-
-public interface DoctorDao extends JpaRepository<Doctor, Integer> {
-
-}
